92ee1bb300cb4644abbcfe9827c7c7801acf716a
Diana Badas <diana.badas@gmail.com>
2019-01-15 13:40:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,6 @@ class String
 
   def count_sentences
     array_whit_sentences = self.split('!', '.', '?')
-    array_whit_sentences = array_whit_sentences.reject! { |s| s.strip.empty? }
     if array_whit_sentences.to_s.length == 0
       return 0
     else
