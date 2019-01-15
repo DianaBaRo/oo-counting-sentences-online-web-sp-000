bad90abbfac622adbc363a9fffa84e15208c0e01
Diana Badas <diana.badas@gmail.com>
2019-01-15 13:24:53 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array_whit_sentences = self.split(/(?<=[?.!])\s*/)
+    array_whit_sentences = array_whit_sentences.reject! { |s| s.strip.empty? }
+    if array_whit_sentences.to_s.length == 0
+      return 0
+    else
+      return array_whit_sentences.length + 1
+    end
   end
 end
